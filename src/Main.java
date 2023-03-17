c33bbf4bb0f643119352669889455d670eda014f
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-17 15:03:02 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // Läs in en fil som lagrat information om personer på formatet:
+        //   Namn Adress
+        //   Ålder Längd(cm) vikt(kg)
+        //
+        // Filtrera ut de personer som har BMI > 30 och spara deras information i en ny fil
+        // BMI = vikt(km) / (längden(m) * längden(m))
+
+
+    }
+}
