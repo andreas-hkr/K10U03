3bc969ff4025c522ad47d5ba16401f5391f78550
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-17 15:21:00 +0100

    Solved (+ignore outfile)

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,36 @@
+import java.io.*;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // Läs in en fil som lagrat information om personer på formatet:
         //   Namn Adress
         //   Ålder Längd(cm) vikt(kg)
         //
         // Filtrera ut de personer som har BMI > 30 och spara deras information i en ny fil
-        // BMI = vikt(km) / (längden(m) * längden(m))
+        // BMI = vikt(kg) / (längden(m) * längden(m))
 
+        BufferedReader inFile = new BufferedReader(new FileReader("persons.txt"));
+        PrintWriter outFile = new PrintWriter(new BufferedWriter(new FileWriter("bmi.txt")));
 
+        while (true) {
+            String row1 = inFile.readLine();
+            if (row1 == null) {
+                break;
+            }
+            String row2 = inFile.readLine();
+            Scanner rowReader = new Scanner(row2);
+            int age = rowReader.nextInt();
+            double height = rowReader.nextInt() / 100.0;
+            double weight = rowReader.nextDouble();
+
+            double bmi = weight / (height * height);
+            if (bmi > 30) {
+                outFile.println(row1);
+                outFile.println(row2);
+            }
+        }
+        inFile.close();
+        outFile.close();
     }
 }
